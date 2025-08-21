253e44ff47334339486c0c919f1b9943156988cb
sEENKS2 <leonel.luchini@gmail.com>
2025-08-21 19:00:46 -0300

    Solucionando errores

---
+++ Clase-02/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('El servidor está funcionando correctamente');
+    });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+}); 
